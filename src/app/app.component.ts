cbefc34c325600464f55c2debe33eb61a2b208e5
ericel <ericel123new@gmail.com>
2017-11-08 20:11:33 +0900

    right side toggle menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { Meta, Title } from '@angular/platform-browser';
-
+import { MatMenuTrigger } from '@angular/material';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild(MatMenuTrigger) trigger: MatMenuTrigger;
+  openMenu(){
+    this.trigger.openMenu();
+  }
 
 }
